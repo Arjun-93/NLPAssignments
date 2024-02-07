2b5b61379f3576e838e1bc961ddbe5fc22b68739
AkshatIIITD <akshat20172@iiitd.ac.in>
2024-02-07 18:37:34 +0530

    final again

--- Assignment1/Task2/A1_2_3helper_2020172.py
+++ Assignment1/Task2/A1_2_3helper_2020172.py
@@ -2,8 +2,8 @@ from data.utils import emotion_scores
 import pandas as pd
 from A1_2_1_2_2020172 import BigramLM
 
-corpus = pd.read_csv('corpus.txt')
-emotion = pd.read_csv('total_emotion_scores.txt')
+corpus = pd.read_csv('Assignment1\data\corpus.txt')
+emotion = pd.read_csv('Assignment1\Task2\\total_emotion_scores.txt')
 # data = pd.concat([corpus, emotion], axis=1)
 
 # Creating a bigram model
@@ -20,6 +20,3 @@ for current_word, next_word_counts in bigram_model.bigramCounts.items():
     for next_word, count in next_word_counts.items():
         with open('bigram_counts.txt', 'a') as f:
             f.write(f"{current_word}-{next_word}-{emotion_scores(next_word)} \n")
-        # print(f"{current_word} {next_word} {count}")
-        # print(emotion_scores(next_word))
-        # print("\n")
